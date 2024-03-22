bdc511d7ad14cd64574169b983d8c3985cac6711
Daniantoo <dion.22058@mhs.unesa.ac.id>
2024-03-22 12:30:45 +0700

    upload

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color correct = Color(0xFF1EE335);
+const Color incorrect = Color(0xFFF52323);
+const Color neutral = Color(0xFFE7E7E7);
+const Color background = Color(0xFF4471E7);
